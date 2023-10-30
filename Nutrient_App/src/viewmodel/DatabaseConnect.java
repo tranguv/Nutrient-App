c4c8a1239cfbd7e9d6a83dd03a74f42a2551008d
tranguv <89886131+tranguv@users.noreply.github.com>
2023-10-29 20:57:26 -0400

    test gitignore

--- Nutrient_App/src/viewmodel/DatabaseConnect.java
+++ Nutrient_App/src/viewmodel/DatabaseConnect.java
@@ -91,9 +91,9 @@ public class DatabaseConnect {
 	}
 	
 	public static void main(String[] args) {
-		 String url = "jdbc:mysql://127.0.0.1:3306/FRISBEE";
+		 String url = "jdbc:mysql://127.0.0.1:3306/database_3311";
 		 String username = "root";
-		 String password = "Qj329032003!";
+		 String password = "Trang@2003";
 
 		 System.out.println("Connecting database...");
 

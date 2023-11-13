0359977288656c4989d985b5c8b86044592c9596
tranguv <89886131+tranguv@users.noreply.github.com>
2023-11-13 00:13:25 -0500

    migrate sql query

--- Nutrient_App/src/server/DataServices/DBQueries.java
+++ Nutrient_App/src/server/DataServices/DBQueries.java
@@ -31,4 +31,27 @@ public class DBQueries {
 			throw new RuntimeException("Error accessing the database", e);
 		}
     }
+
+	public static boolean signUpUser(String username, String password) {
+        DBConfig dbConfig = new DBConfig();
+
+		try (Connection connection = DriverManager.getConnection(dbConfig.getUrl(), dbConfig.getUsername(), dbConfig.getPassword())) {
+			String sql = "SELECT * FROM USER WHERE username = ? AND user_password = ?";
+			try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+				preparedStatement.setString(1, username);
+				preparedStatement.setString(2, password);
+
+				try (ResultSet resultSet = preparedStatement.executeQuery()) {
+					if (resultSet.next()) { // If a row is returned, the credentials are valid
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error accessing the database", e);
+		}
+    }
 }

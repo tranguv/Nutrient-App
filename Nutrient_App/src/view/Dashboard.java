844e8f007f091be4767932022ab7536eb81f35ac
chiemgiabaost <bao2003@my.yorku.ca>
2023-11-14 00:40:11 -0500

    UI for dashboard has improved, but add meal and add excersice still not working

--- Nutrient_App/src/view/Dashboard.java
+++ Nutrient_App/src/view/Dashboard.java
@@ -10,43 +10,105 @@ import javax.swing.*;
 
 public class Dashboard extends JFrame{
 
-    private JTextField mealTypeField, ingredientNameField, quantityField, unitField,
-            exerciseTypeField, durationField, intensityField ;
     private JComboBox<String> mealTypeComboBox, ingredientCombo;
-    private JTextArea mealsTextArea, exercisesTextArea;
+    private JTextField quantityField, unitField, exerciseTypeField, durationField, intensityField;
+    private JTextArea mealsTextArea, exercisesTextArea, historyTextArea;
     private JButton addMealButton, addExerciseButton, saveLogButton;
-    private JTextArea historyTextArea;
 
     public Dashboard() {
-
         setTitle("Nutrifit App");
-        setSize(1000, 600); // Increased width for larger text fields
+        setSize(1000, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout(10, 10)); // Add spacing between components
+        setLayout(new BorderLayout(10, 10));
 
-        JPanel inputPanel = new JPanel(new GridBagLayout());
+        // Create the panel for meal inputs
+        JPanel mealPanel = new JPanel(new GridBagLayout());
+        mealPanel.setBorder(BorderFactory.createTitledBorder("Meal Details"));
+        addMealFields(mealPanel);
 
-        inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Add padding
+        // Create the panel for exercise inputs
+        JPanel exercisePanel = new JPanel(new GridBagLayout());
+        exercisePanel.setBorder(BorderFactory.createTitledBorder("Exercise Details"));
+        addExerciseFields(exercisePanel);
 
+        // Combine meal and exercise panels
+        JPanel combinedInputPanel = new JPanel();
+        combinedInputPanel.setLayout(new BoxLayout(combinedInputPanel, BoxLayout.Y_AXIS));
+        combinedInputPanel.add(mealPanel);
+        combinedInputPanel.add(exercisePanel);
+
+        // Panel for buttons
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        addMealButton = new JButton("Add Meal");
+        addExerciseButton = new JButton("Add Exercise");
+        buttonPanel.add(addMealButton);
+        buttonPanel.add(addExerciseButton);
+
+        // Log panel
+        JPanel logPanel = new JPanel(new GridLayout(1, 2, 10, 10));
+        logPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mealsTextArea = new JTextArea();
+        exercisesTextArea = new JTextArea();
+        logPanel.add(new JScrollPane(mealsTextArea));
+        logPanel.add(new JScrollPane(exercisesTextArea));
+
+        // Save button panel
+        JPanel savePanel = new JPanel(new FlowLayout());
+        saveLogButton = new JButton("Save Log");
+        savePanel.add(saveLogButton);
+
+        // History text area
+        historyTextArea = new JTextArea();
+        historyTextArea.setEditable(false);
+        JScrollPane historyScrollPane = new JScrollPane(historyTextArea);
+        historyScrollPane.setPreferredSize(new Dimension(980, 100));
+
+        // History panel
+        JPanel historyPanel = new JPanel(new BorderLayout());
+        historyPanel.add(historyScrollPane, BorderLayout.CENTER);
+        historyPanel.setBorder(BorderFactory.createTitledBorder("History"));
+
+        // Add panels to the frame
+        add(combinedInputPanel, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
+        add(logPanel, BorderLayout.SOUTH);
+        add(savePanel, BorderLayout.AFTER_LAST_LINE);
+        add(historyPanel, BorderLayout.AFTER_LAST_LINE);
+
+        // Add action listeners
+//        addMealButton.addActionListener(e -> addMeal());
+//        addExerciseButton.addActionListener(e -> addExercise());
+//        saveLogButton.addActionListener(e -> saveLog());
+
+        setVisible(true);
+    }
+
+    private void addMealFields(JPanel panel) {
+        panel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5); // Add some padding between components
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
+        // Meal Type Label
         gbc.gridx = 0;
-        gbc.gridy++;
-        inputPanel.add(new JLabel("Meal Type:"), gbc);
-        String[] mealTypes = {"Breakfast", "Lunch", "Dinner", "Snack"};
-        mealTypeComboBox = new JComboBox<>(mealTypes);
+        gbc.gridy = 0;
+        panel.add(new JLabel("Meal Type:"), gbc);
+
+        // Meal Type ComboBox
+        gbc.gridx++;
+        mealTypeComboBox = new JComboBox<>(new String[]{"Breakfast", "Lunch", "Dinner", "Snack"});
         mealTypeComboBox.setPreferredSize(new Dimension(200, 30));
-        gbc.gridx = 1;
-        inputPanel.add(mealTypeComboBox, gbc);
+        panel.add(mealTypeComboBox, gbc);
 
 
 
-        gbc.gridx = 0;
-        gbc.gridy++;
-        inputPanel.add(new JLabel("Ingredient Name:"), gbc);
-        String[] ingredients = {
-                "Dairy and Egg Products",
+        // Ingredient Name Label
+        gbc.gridx++;
+        panel.add(new JLabel("Ingredient Name:"), gbc);
+
+        // Ingredient ComboBox
+        gbc.gridx++;
+        ingredientCombo = new JComboBox<>(new String[]{  "Dairy and Egg Products",
                 "Spices and Herbs",
                 "Babyfoods",
                 "Fats and Oils",
@@ -68,152 +130,185 @@ public class Dashboard extends JFrame{
                 "Cereals, Grains, and Pasta",
                 "Fast Foods",
                 "Mixed Dishes",
-                "Snacks"
-        };
-        ingredientCombo = new JComboBox<>(ingredients);
-        gbc.gridx = 1;
+                "Snacks"});
+        ingredientCombo.setPreferredSize(new Dimension(200, 30));
+        panel.add(ingredientCombo, gbc);
 
-        ingredientCombo.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(ingredientCombo, gbc);
+        // Quantity Label
+        gbc.gridx++;
+        panel.add(new JLabel("Quantity:"), gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy++;
-        inputPanel.add(new JLabel("Quantity:"), gbc);
-        gbc.gridx = 1;
+        // Quantity Field
+        gbc.gridx++;
         quantityField = new JTextField();
-        quantityField.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(quantityField, gbc);
+        quantityField.setPreferredSize(new Dimension(100, 30)); // Adjusted size to provide space
+        panel.add(quantityField, gbc);
 
+        // Unit Label
+        gbc.gridx++;
+        panel.add(new JLabel("Unit:"), gbc);
+
+        // Unit Field
+        gbc.gridx++;
+        unitField = new JTextField();
+        unitField.setPreferredSize(new Dimension(100, 30)); // Adjusted size to provide space
+        panel.add(unitField, gbc);
+
+        // Add Ingredient Button
+        JButton addIngredientButton = new JButton("+");
+        addIngredientButton.setPreferredSize(new Dimension(50, 30));
+        gbc.gridx++; // Move to the next column
+        gbc.gridwidth = 1; // Reset grid width if previously altered
+        gbc.fill = GridBagConstraints.HORIZONTAL; // Ensure the button fills its grid cell horizontally
+        panel.add(addIngredientButton, gbc);
+        // Action listener for "Add Ingredient" button
+        addIngredientButton.addActionListener(e -> {
+            addIngredientFields(panel);
+            panel.revalidate();
+            panel.repaint();
+        });
+    }
+
+    private void addIngredientFields(JPanel panel) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        int nextRow = panel.getComponentCount() / 8; // Assuming 8 components per ingredient row
+
+        // Ingredient Name Label
+        gbc.gridx = 2;
+        gbc.gridy = nextRow;
+        panel.add(new JLabel("Ingredient Name:"), gbc);
+
+        // Ingredient ComboBox
+        gbc.gridx++;
+        JComboBox<String> newIngredientCombo = new JComboBox<>(new String[]{  "Dairy and Egg Products",
+                "Spices and Herbs",
+                "Babyfoods",
+                "Fats and Oils",
+                "Poultry Products",
+                "Soups, Sauces and Gravies",
+                "Sausages and Luncheon Meats",
+                "Breakfast Cereals",
+                "Fruits and Fruit Juices",
+                "Pork Products",
+                "Vegetables and Vegetable Products",
+                "Nuts and Seeds",
+                "Beef Products",
+                "Beverages",
+                "Finfish and Shellfish Products",
+                "Legumes and Legume Products",
+                "Lamb, Veal, and Game",
+                "Baked Products",
+                "Sweets",
+                "Cereals, Grains, and Pasta",
+                "Fast Foods",
+                "Mixed Dishes",
+                "Snacks"});
+        newIngredientCombo.setPreferredSize(new Dimension(200, 30));
+        panel.add(newIngredientCombo, gbc);
+
+        // Quantity Label
+        gbc.gridx++;
+        panel.add(new JLabel("Quantity:"), gbc);
+
+        // Quantity Field
+        gbc.gridx++;
+        JTextField newQuantityField = new JTextField();
+        newQuantityField.setPreferredSize(new Dimension(100, 30));
+        panel.add(newQuantityField, gbc);
+
+        // Unit Label
+        gbc.gridx++;
+        panel.add(new JLabel("Unit:"), gbc);
+
+        // Unit Field
+        gbc.gridx++;
+        JTextField newUnitField = new JTextField();
+        newUnitField.setPreferredSize(new Dimension(100, 30));
+        panel.add(newUnitField, gbc);
+    }
+
+    private void addExerciseFields(JPanel panel) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        // Exercise Type
+        panel.add(new JLabel("Exercise Type:"), gbc);
+        exerciseTypeField = new JTextField();
+        gbc.gridx = 1;
+        panel.add(exerciseTypeField, gbc);
+        exerciseTypeField.setPreferredSize(new Dimension(200,30));
+        // Duration
         gbc.gridx = 0;
         gbc.gridy++;
-        inputPanel.add(new JLabel("Unit:"), gbc);
-        gbc.gridx = 1;
-        unitField = new JTextField();
-        unitField.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(unitField, gbc);
-
-        gbc.gridx = 2; // Move to the next column
-        gbc.gridy = 0; // Reset the row
-        gbc.fill = GridBagConstraints.NONE; // Reset the fill property
-
-        inputPanel.add(new JLabel("Exercise Type:"), gbc);
-        gbc.gridx = 3;
-        exerciseTypeField = new JTextField();
-        exerciseTypeField.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(exerciseTypeField, gbc);
-
-        gbc.gridx = 2;
-        gbc.gridy++;
-        inputPanel.add(new JLabel("Duration:"), gbc);
-        gbc.gridx = 3;
+        panel.add(new JLabel("Duration:"), gbc);
         durationField = new JTextField();
-        durationField.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(durationField, gbc);
+        gbc.gridx = 1;
+        panel.add(durationField, gbc);
+        durationField.setPreferredSize(new Dimension(200,30));
 
-        gbc.gridx = 2;
+        // Intensity
+        gbc.gridx = 0;
         gbc.gridy++;
-        inputPanel.add(new JLabel("Intensity:"), gbc);
-        gbc.gridx = 3;
+        panel.add(new JLabel("Intensity:"), gbc);
         intensityField = new JTextField();
-        intensityField.setPreferredSize(new Dimension(200, 30)); // Set preferred size
-        inputPanel.add(intensityField, gbc);
-
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        addMealButton = new JButton("Add Meal");
-        addExerciseButton = new JButton("Add Exercise");
-        buttonPanel.add(addMealButton);
-        buttonPanel.add(addExerciseButton);
-
-        JPanel logPanel = new JPanel(new GridLayout(1, 2, 10, 10)); // 1 row, 2 columns (variable columns)
-        logPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        mealsTextArea = new JTextArea();
-        exercisesTextArea = new JTextArea();
-        logPanel.add(new JScrollPane(mealsTextArea));
-        logPanel.add(new JScrollPane(exercisesTextArea));
-
-        JPanel savePanel = new JPanel(new FlowLayout());
-        saveLogButton = new JButton("Save Log");
-        savePanel.add(saveLogButton);
-
-        historyTextArea = new JTextArea();
-        historyTextArea.setEditable(false); // Make it read-only
-        JScrollPane historyScrollPane = new JScrollPane(historyTextArea); // Add scroll pane
-        historyScrollPane.setPreferredSize(new Dimension(980, 100)); // Set preferred size
-
-        // Adjust layout
-        JPanel historyPanel = new JPanel(new BorderLayout());
-        historyPanel.add(historyScrollPane, BorderLayout.CENTER);
-        historyPanel.setBorder(BorderFactory.createTitledBorder("History"));
-        add(historyPanel, BorderLayout.AFTER_LAST_LINE);
-
-
-        JPanel southPanel = new JPanel(new BorderLayout());
-        add(inputPanel, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.CENTER);
-        add(logPanel, BorderLayout.SOUTH);
-        add(savePanel, BorderLayout.SOUTH);
-
-        addMealButton.addActionListener(e -> addMeal());
-        addExerciseButton.addActionListener(e -> addExercise());
-        saveLogButton.addActionListener(e -> saveLog());
-
-        setVisible(true);
+        gbc.gridx = 1;
+        panel.add(intensityField, gbc);
+        intensityField.setPreferredSize(new Dimension(200,30));
     }
-    private void updateHistory(String newEntry) {
-        // Format the current date
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String formattedDate = currentDate.format(formatter);
+//    private void updateHistory(String newEntry) {
+//        // Format the current date
+//        LocalDate currentDate = LocalDate.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        String formattedDate = currentDate.format(formatter);
+//
+//        // Append the date and new entry followed by a newline to the history text area
+//        historyTextArea.append(formattedDate + " : " + newEntry + "\n");
+//
+//        // Scroll the history text area to the bottom
+//        historyTextArea.setCaretPosition(historyTextArea.getDocument().getLength());
+//    }
+//
+//    private void addMeal() {
+//        String selectedMealType = (String) mealTypeComboBox.getSelectedItem();
+//        String selectedIngredient = (String) ingredientCombo.getSelectedItem();
+//        String mealInfo = String.format("Meal Type: %s, Ingredients: %s, Quantity: %s, Unit: %s",
+//                selectedMealType, selectedIngredient,
+//                quantityField.getText(), unitField.getText());
+//        mealsTextArea.append(mealInfo + "\n");
+//        updateHistory("Added Meal: " + mealInfo);
+//        clearMealFields();
+//    }
+//    private void clearMealFields() {
+//        // Removed the mealTypeField line as it's no longer in use
+//        ingredientNameField.setText("");
+//        quantityField.setText("");
+//        unitField.setText("");
+//    }
+//    private void addExercise() {
+//        String exerciseInfo = String.format("Exercise Type : %s, Duration: %s minutes, Intensity: %s",
+//                exerciseTypeField.getText(), durationField.getText(), intensityField.getText());
+//        exercisesTextArea.append(exerciseInfo + "\n");
+//        updateHistory("Added Exercise : " + exerciseInfo);
+//        clearExerciseFields();
+//    }
+//
+//
+//    private void saveLog() {
+//        System.out.println("Meals Log:\n" + mealsTextArea.getText());
+//        System.out.println("Exercises Log:\n" + exercisesTextArea.getText());
+//    }
 
-        // Append the date and new entry followed by a newline to the history text area
-        historyTextArea.append(formattedDate + " : " + newEntry + "\n");
-
-        // Scroll the history text area to the bottom
-        historyTextArea.setCaretPosition(historyTextArea.getDocument().getLength());
-    }
-
-    private void addMeal() {
-        String selectedMealType = (String) mealTypeComboBox.getSelectedItem();
-        String selectedIngredient = (String) ingredientCombo.getSelectedItem();
-        String mealInfo = String.format("Meal Type: %s, Ingredients: %s, Quantity: %s, Unit: %s",
-                selectedMealType, selectedIngredient,
-                quantityField.getText(), unitField.getText());
-        mealsTextArea.append(mealInfo + "\n");
-        updateHistory("Added Meal: " + mealInfo);
-        clearMealFields();
-    }
-    private void clearMealFields() {
-        // Removed the mealTypeField line as it's no longer in use
-        ingredientNameField.setText("");
-        quantityField.setText("");
-        unitField.setText("");
-    }
-    private void addExercise() {
-        String exerciseInfo = String.format("Exercise Type : %s, Duration: %s minutes, Intensity: %s",
-                exerciseTypeField.getText(), durationField.getText(), intensityField.getText());
-        exercisesTextArea.append(exerciseInfo + "\n");
-        updateHistory("Added Exercise : " + exerciseInfo);
-        clearExerciseFields();
-    }
-
-
-    private void saveLog() {
-        System.out.println("Meals Log:\n" + mealsTextArea.getText());
-        System.out.println("Exercises Log:\n" + exercisesTextArea.getText());
-    }
 
 
 
-    private void clearExerciseFields() {
-        mealTypeField.setText("");
-        exerciseTypeField.setText("");
-        durationField.setText("");
-        intensityField.setText("");
-    }
 
     public static void main(String[] args) {
         Dashboard d = new Dashboard();
     }
-	
+
 }

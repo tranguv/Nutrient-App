2735b55c6916b6a400eacfdabfb0352ffc6fd701
chiemgiabaost <bao2003@my.yorku.ca>
2023-11-22 18:50:17 -0500

    NOthing

--- Nutrient_App/src/main/Main.java
+++ Nutrient_App/src/main/Main.java
@@ -32,13 +32,13 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws ParseException {
-//		try {
-//			MainApplication mainApp = new MainApplication();
-//			mainApp.run();
-//		} catch (Exception e) {
-//			System.err.println("Unexpected exception: " + e.getMessage());
-//			// Handle other unexpected exceptions
-//		}
+		try {
+			MainApplication mainApp = new MainApplication();
+			mainApp.run();
+		} catch (Exception e) {
+			System.err.println("Unexpected exception: " + e.getMessage());
+			// Handle other unexpected exceptions
+		}
 //		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 //		Date specificDate1 = dateFormat.parse("2023-11-21 12:00:00");
 
@@ -46,13 +46,13 @@ public class Main {
 //		java.util.Calendar calendar = java.util.Calendar.getInstance();
 //		calendar.set(2023, 10, 21, 12, 0, 0);
 //		Date specificDate2 = calendar.getTime();
-		Date date = DateQueries.getDatebyUserID(32);
-		List<DailyNutrientIntakeViz> nutrientdataList = DailyNutrientIntakeViz.getNutrientValConsumed(32, date);
-		List<DailyNutrientIntakeViz> getTop5Nutrient = DailyNutrientIntakeViz.getTop5Nutrient(nutrientdataList);
-		List<DailyNutrientIntakeViz> getRemainNutrient = DailyNutrientIntakeViz.getRemainNutrient(nutrientdataList);
-		for (int i = 0; i < getRemainNutrient.size(); i++ ) {
-			System.out.println(getRemainNutrient.get(i).getTotalNutrientAmt());
-		}
+//		Date date = DateQueries.getDatebyUserID(32);
+//		List<DailyNutrientIntakeViz> nutrientdataList = DailyNutrientIntakeViz.getNutrientValConsumed(32, date);
+//		List<DailyNutrientIntakeViz> getTop5Nutrient = DailyNutrientIntakeViz.getTop5Nutrient(nutrientdataList);
+//		List<DailyNutrientIntakeViz> getRemainNutrient = DailyNutrientIntakeViz.getRemainNutrient(nutrientdataList);
+//		for (int i = 0; i < getRemainNutrient.size(); i++ ) {
+//			System.out.println(getRemainNutrient.get(i).getTotalNutrientAmt());
+//		}
 
 	}
 }
